3fd2205e0bfb5c172c93ce7c92837480eabde9a9
Opposum13 <vika.petruk@i.ua>
2017-09-15 20:08:09 +0200

    Homework2
    Completed

--- Assets/HeroRabit.cs
+++ Assets/HeroRabit.cs
@@ -7,6 +7,11 @@ public class HeroRabit : MonoBehaviour {
     public float speed = 1;
 
     Rigidbody2D myBody = null;
+    bool isGrounded = false;
+    bool JumpActive = false;
+    float JumpTime = 0f;
+    public float MaxJumpTime = 2f;
+    public float JumpSpeed = 2f;
 
     // Use this for initialization
     void Start() {
@@ -24,13 +29,34 @@ public class HeroRabit : MonoBehaviour {
             Vector2 vel = myBody.velocity;
             vel.x = value * speed;
             myBody.velocity = vel;
-            animator.SetBool("run", true);
-            animator.SetBool("idle", false);
+            if (this.isGrounded)
+            {
+                animator.SetBool("jump", false);
+                animator.SetBool("run", true);
+                animator.SetBool("idle", false);
+            }
+            else
+            {
+                animator.SetBool("jump", true);
+                animator.SetBool("run", false);
+                animator.SetBool("idle", false);
+            }
         }
         else
         {
-            animator.SetBool("run", false);
-            animator.SetBool("idle", true);
+            
+            if (this.isGrounded)
+            {
+                animator.SetBool("jump", false);
+                animator.SetBool("run", false);
+                animator.SetBool("idle", true);
+            }
+            else
+            {
+                animator.SetBool("jump", true);
+                animator.SetBool("run", false);
+                animator.SetBool("idle", false);
+            }
         }
 
 
@@ -38,11 +64,79 @@ public class HeroRabit : MonoBehaviour {
         if (value < 0)
         {
             sr.flipX = true;
+            if (this.isGrounded)
+            {
+                animator.SetBool("jump", false);
+                animator.SetBool("run", true);
+                animator.SetBool("idle", false);
+            }
+            else
+            {
+                animator.SetBool("jump", true);
+                animator.SetBool("run", false);
+                animator.SetBool("idle", false);
+            }
+
         }
         else if (value > 0)
         {
             sr.flipX = false;
+            if (this.isGrounded)
+            {
+                animator.SetBool("jump", false);
+                animator.SetBool("run", true);
+                animator.SetBool("idle", false);
+            }
+            else
+            {
+                animator.SetBool("jump", true);
+                animator.SetBool("run", false);
+                animator.SetBool("idle", false);
+            }
         }
+
+        Vector3 from = transform.position + Vector3.up * 0.3f;
+        Vector3 to = transform.position + Vector3.down * 0.1f;
+        int layer_id = 1 << LayerMask.NameToLayer("Ground");
+        //Перевіряємо чи проходить лінія через Collider з шаром Ground
+        RaycastHit2D hit = Physics2D.Linecast(from, to, layer_id);
+        if (hit)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+        //Намалювати лінію (для розробника)
+        Debug.DrawLine(from, to, Color.red);
+
+
+        //Якщо кнопка тільки що натислась
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            this.JumpActive = true;
+        }
+        if (this.JumpActive)
+        {
+            //Якщо кнопку ще тримають
+            if (Input.GetButton("Jump"))
+            {
+                this.JumpTime += Time.deltaTime;
+                if (this.JumpTime < this.MaxJumpTime)
+                {
+                    Vector2 vel = myBody.velocity;
+                    vel.y = JumpSpeed * (1.0f - JumpTime / MaxJumpTime);
+                    myBody.velocity = vel;
+                }
+            }
+            else
+            {
+                this.JumpActive = false;
+                this.JumpTime = 0;
+            }
+        }
+
         
 
     }
