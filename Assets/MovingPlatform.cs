1633d2b1da9ed3a67c3ab85194e903c8a23a3893
Opposum13 <vika.petruk@i.ua>
2017-09-27 19:58:20 +0200

    Prefab and platform
    rabit doesnt move with platform

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour {
+
+    Vector3 posA;
+    Vector3 posB;
+    Vector3 nextPos;
+
+    [SerializeField]
+    private float speed;
+
+    private float timeToWait;
+
+    [SerializeField]
+    private Transform childTransform;
+
+    [SerializeField]
+    private Transform transformB;
+
+    // Use this for initialization
+    void Start () {
+        posA = childTransform.localPosition;
+        posB = transformB.localPosition;
+        nextPos = posB;
+        timeToWait = 4;
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        Move();
+    }
+
+    private void Move()
+    {
+        childTransform.localPosition = Vector3.MoveTowards(childTransform.localPosition, nextPos, speed * Time.deltaTime);
+        if (Vector3.Distance(childTransform.localPosition, nextPos) <= 0.1)
+        {
+            timeToWait -= Time.deltaTime;
+            if(timeToWait <= 0)
+            {
+                ChangeDestination();
+                timeToWait = 4;
+            }
+            
+        }
+
+    }
+
+    private void ChangeDestination()
+    {
+        nextPos = nextPos != posA ? posA : posB;
+    }
+}
